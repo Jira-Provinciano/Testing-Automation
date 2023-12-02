a8671b02ca325ce3c173192b480fec4f62bd5e5a
SigmaTaurus <juan94nak@gmail.com>
2023-12-01 23:12:07 -0300

    update test_PTF_3.py

--- test_PTF_3.py
+++ test_PTF_3.py
@@ -38,11 +38,112 @@ def test_region_ninguna(driver):
         'Buenos Aires'
         ]
     assert data_prov == expected_prov, f"Los datos de la Grilla no son los esperados"
+
+def test_region_centro(driver):
+    select = driver.find_element(By.XPATH, '//*[@id="region"]/option[3]')
+    select.click()
+    table = driver.find_element(By.XPATH,'//*[@id="data"]/tbody')
+    filas = table.find_elements(By.TAG_NAME,'tr')
+    lists =[]
+    for fila in filas:
+        list = []
+        celdas = fila.find_elements(By.TAG_NAME,'td')
+        for celda in celdas:
+            celda = celda.text
+            list.append(celda)
+        lists.append(list)    
+    data_prov = []
+    for lista in lists:
+        data_prov.append(lista[1])
+    expected_prov = [
+        'Córdoba',
+        'Entre Ríos',
+        'Santa Fe'
+        ]
+    assert data_prov == expected_prov, f"Los datos de la Grilla no son los esperados"
+
+def test_region_norte(driver):
+    select = driver.find_element(By.XPATH, '//*[@id="region"]/option[4]')
+    select.click()
+    table = driver.find_element(By.XPATH,'//*[@id="data"]/tbody')
+    filas = table.find_elements(By.TAG_NAME,'tr')
+    lists =[]
+    for fila in filas:
+        list = []
+        celdas = fila.find_elements(By.TAG_NAME,'td')
+        for celda in celdas:
+            celda = celda.text
+            list.append(celda)
+        lists.append(list)    
+    data_prov = []
+    for lista in lists:
+        data_prov.append(lista[1])
+    expected_prov = [
+        'Catamarca',
+        'Chaco',
+        'Corrientes',
+        'Formosa',
+        'Jujuy',
+        'Misiones',
+        'Salta',
+        'Santiago del Estero',
+        'Tucumán'
+        ]
+    assert data_prov == expected_prov, f"Los datos de la Grilla no son los esperados"
         
-    
+def test_region_cuyo(driver):
+    select = driver.find_element(By.XPATH, '//*[@id="region"]/option[5]')
+    select.click()
+    table = driver.find_element(By.XPATH,'//*[@id="data"]/tbody')
+    filas = table.find_elements(By.TAG_NAME,'tr')
+    lists =[]
+    for fila in filas:
+        list = []
+        celdas = fila.find_elements(By.TAG_NAME,'td')
+        for celda in celdas:
+            celda = celda.text
+            list.append(celda)
+        lists.append(list)    
+    data_prov = []
+    for lista in lists:
+        data_prov.append(lista[1])
+    expected_prov = [
+        'La Rioja',
+        'Mendoza',
+        'San Juan',
+        'San Luis'
+        ]
+    assert data_prov == expected_prov, f"Los datos de la Grilla no son los esperados"
+
+def test_region_patagonica(driver):
+    select = driver.find_element(By.XPATH, '//*[@id="region"]/option[6]')
+    select.click()
+    table = driver.find_element(By.XPATH,'//*[@id="data"]/tbody')
+    filas = table.find_elements(By.TAG_NAME,'tr')
+    lists =[]
+    for fila in filas:
+        list = []
+        celdas = fila.find_elements(By.TAG_NAME,'td')
+        for celda in celdas:
+            celda = celda.text
+            list.append(celda)
+        lists.append(list)    
+    data_prov = []
+    for lista in lists:
+        data_prov.append(lista[1])
+    expected_prov = [
+        'Chubut',
+        'La Pampa',
+        'Neuquén',
+        'Río Negro',
+        'Santa Cruz',
+        'Tierra del Fuego, Antártida e Islas del Atlántico Sur']
+    assert data_prov == expected_prov, f"Los datos de la Grilla no son los esperados"
+
+
 
 #x = driver()
-#test_region_ninguna(x)
+#test_region_patagonica(x)
 #x.close()
 
     
